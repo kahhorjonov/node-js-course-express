a6b839bf6604bb8dfc63940edf7c5161c1e7fade
kahhorjonov <akbarjon3028@gmail.com>
2022-06-04 18:31:56 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const Joi = require("joi");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// app.get();
+// app.post();
+// app.put();
+// app.delete();
+
+let courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+  { id: 4, name: "course4" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello Friend");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.post("/api/courses", (req, res) => {
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("The course with the given ID wasn't found");
+  }
+  const { error } = validateCourse(req.body);
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  let course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("The course with the given ID wasn't found");
+  }
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({ name: Joi.string().min(3).required() });
+  return schema.validate(course);
+}
+
+// app.get("/api/courses/:id", (req, res) => {
+//   res.send(req.params.id);
+// });
+
+app.get("/api/posts/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
