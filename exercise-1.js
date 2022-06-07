c91b36f1ba79779ea54aed5591089c88e81de5d8
kahhorjonov <akbarjon3028@gmail.com>
2022-06-07 11:32:28 +0300

    exercises finished

---
+++ exercise-1.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/mongo-exercises")
+  .then(() => console.log("Connected to Mongo-Exercises"))
+  .catch((err) => console.error("Connection Error", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  tags: [String],
+  date: Date,
+  author: String,
+  isPublished: Boolean,
+  price: Number,
+  __v: Number,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+const getCourses = async () => {
+  const result = await Course.find({ isPublished: true, tags: "backend" })
+    .sort("name")
+    .select({ name: 1, author: 1 });
+  console.log(result);
+};
+
+// getCourses();
+
+const getCourses2 = async () => {
+  const result = await await Course.find({ isPublished: true })
+    .find({ tags: { $in: ["frontend", "backend"] } })
+    .sort("-price")
+    .select(name, author, price);
+  console.log(result);
+};
+
+// getCourses2();
+
+const getCourses3 = async () => {
+  const result = await await Course.find({ isPublished: true }).or([
+    {
+      price: { $gte: 15 },
+    },
+    { name: /.*by.*/ },
+  ]);
+
+  console.log(result);
+};
+
+getCourses3();
---
+++ index.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Couldn't connect to MongoDB...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+const createCourse = async () => {
+  const course = new Course({
+    name: "Ultimate Redux course",
+    author: "Mosh",
+    tags: ["redux", "frontend"],
+    isPublished: false,
+  });
+
+  const result = await course.save();
+  console.log(result);
+};
+
+const getCourses = async () => {
+  // Comparison Queries -->
+
+  // eq (equal)
+  //   ne (not equal)
+  //   gt (greater than)
+  //   gte (greater than or equal to)
+  //   lt (less than)
+  //   lte (less than or equal to)
+  //   in
+  //   nin (not in)
+
+  //   <-- Comparison Queries
+
+  //   Logical Queries -->
+  //   or
+  //   and
+
+  //   <-- Logical Queries
+
+  const pageNumber = 2;
+  const pageSize = 10;
+
+  //   Example
+  //   //api/courses?pageNumber=2&pageSize=10
+
+  //   Regular Expression -->
+  const result = await Course
+    //   Starts With
+    .find({ author: /^Mosh/ })
+
+    // Ends With
+    .find({ auhtor: /Hamedani$/i })
+
+    // Contains With
+    .find({ author: /.*Mosh.*/ })
+
+    // .or([{ author: "Shah" }, { isPublished: "false" }])
+    // .and([{}, {}])
+    //   .find({ price: { $in: [10, 15, 20] } })
+    //   .find({ price: { $gte: 10, $lte: 20 } })
+    // .find({ author: "Mosh", isPublished: false })
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 })
+    .count();
+  console.log(result);
+};
+
+getCourses();
+// createCourse();
